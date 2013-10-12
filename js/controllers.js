0a10b38583944f3f0b4cb249d32268700216e7b3
blacked <alexey.dubkov@gmail.com>
2013-10-12 18:30:07 +0400

    Add checking for undefined

--- js/controllers.js
+++ js/controllers.js
@@ -74,14 +74,16 @@ angular.module('shriApp.controllers', []).run(['$rootScope', '$http', function(r
   * Update View function
   **/
   var updateView = function(){
-    // Initialize content
-    var e = sc.data.menu[lc.path()];    
-    sc.caption = e.caption;
-    sc.content = e.content;
+    if (sc.data !== undefined) {
+      // Initialize content
+      var e = sc.data.menu[lc.path()];    
+      sc.caption = e.caption;
+      sc.content = e.content;
 
-    // Set page title 
-    sc.title = sc.data.title;
-    sc.$emit('setPageTitle', sc.title);
+      // Set page title 
+      sc.title = sc.data.title;
+      sc.$emit('setPageTitle', sc.title);
+    }
   }
   updateView();
 
